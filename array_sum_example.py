5c63c67e30e21cd90bfcae4dcc1e0dcca05bc774
Hector Santiago <52340742+santiago0072002@users.noreply.github.com>
2021-01-17 10:25:43 -0500

    Add files via upload

---
+++ array_sum_example.py
@@ -0,0 +1,5 @@
+def array_sum(array_list): # define the function that takes a paremeter
+    return sum(array_list) # return back to the caller everything that was passed to it added
+
+list1 = [1,2,3,4,5]         # declare a list
+print(array_sum(list1))     # caller, show it in the console. 
