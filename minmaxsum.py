212f985d06e689eb4447efc66272b704b20cc339
Hector Santiago <52340742+santiago0072002@users.noreply.github.com>
2021-01-18 16:17:45 -0500

    Add files via upload

---
+++ minmaxsum.py
@@ -0,0 +1,8 @@
+# hacker rank mix max me warming up. 
+# 
+def miniMaxSum(arr):# pass an array of numbers
+    print(sum(arr) - max(arr), sum(arr) - min(arr)) # using python build it libraries you can just substract the max and get the min and substract the min to get the maximun. 
+
+list2 = [1, 2, 3, 4 , 5]
+
+miniMaxSum(list2)
