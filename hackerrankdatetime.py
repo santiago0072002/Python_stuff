36f8454b67f9b8c06236970f3c01c8f7c6e98147
Hector Santiago <52340742+santiago0072002@users.noreply.github.com>
2021-01-19 15:03:56 -0500

    Add files via upload

--- hackerrankdatetime.py
+++ hackerrankdatetime.py
@@ -3,10 +3,10 @@
 def timeConversion(s):
     last_two = s[-2:]# this will get the last two characters from the string./ string slicing
     if last_two == "PM" and s[:2]!="12":# if the last two == pm and the first two not equal 12
-        s = str(12 + int(s[:2])) + s[2:]
-    if last_two == "AM" and s[:2] == "12": 
-        s = "00" + s[2:]
-    return s[:-2]
+        s = str(12 + int(s[:2])) + s[2:] # convert s to 12 plus the first 2 chars, and the rest of the characters after the first two
+    if last_two == "AM" and s[:2] == "12": # if the last two is AM and the first two == 12
+        s = "00" + s[2:] # the string is 00 and the rest of the slice after the first two chars
+    return s[:-2] # anyother option just return everything minus the last two chars. 
 
 timeconv="07:05:45PM"
 print(timeConversion(timeconv))
